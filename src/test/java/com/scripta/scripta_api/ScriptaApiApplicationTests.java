0c0741407f579ff6353b90a4fb601e0dd0e9bd7a
Jason de Morais <jasoncamposfilho@hotmail.com>
2024-09-24 16:04:05 -0300

    Started Spring Project

---
+++ src/test/java/com/scripta/scripta_api/ScriptaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scripta.scripta_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
