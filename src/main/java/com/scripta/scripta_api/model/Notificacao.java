37a571ebaba4d133933b08d8f52eacac59ac4474
Jason de Morais <jasoncamposfilho@hotmail.com>
2024-09-25 09:49:39 -0300

    Feat: arquivos obrigatorios criados

---
+++ src/main/java/com/scripta/scripta_api/model/Notificacao.java
@@ -0,0 +1,21 @@
+package com.scripta.scripta_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Notificacao")
+public class Notificacao {
+    
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id_notificacao")
+    private Long notificacaoID;
+
+
+}
